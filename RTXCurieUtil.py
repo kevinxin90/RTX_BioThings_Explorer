6f70be20463abebf37199dba9dbe5510931caf70
Yao Yao <yaoy.cs@gmail.com>
2018-05-22 15:58:17 -0400

    add RTXCurieUtil module to convert RTX CURIE-form ID to short form

---
+++ RTXCurieUtil.py
@@ -0,0 +1,57 @@
+__all__ = ['convert_rtx_curie_id_to_bt_explorer_input']
+
+_input_prefix_map = {
+    # metabolite
+    "KEGG": "kegg.compound",
+    # protein
+    "UniProtKB": "uniprot",
+    # anatomical_entity
+    "UBERON": "uberon",
+    # TODO category?
+    "CL": "cl",
+    # gene_ontolog
+    "GO": "go",
+    # disease
+    "DOID": "do",
+    # genetic_condition
+    "OMIM": "omim.disease",
+    # phenotypic_feature
+    "HP": "hp",
+    # "AQTLTrait": None,
+    # microRNA
+    "NCBIGene": "ncbigene",
+    # pathway
+    "REACT": "reactome.pathway",
+    # checmical_substance
+    "ChEMBL": "chembl.compound"
+}
+
+
+def convert_rtx_curie_id_to_bt_explorer_input(curie_id):
+    """
+    KEGG --> ^C\d+$, equivalent to splitting by colon. E.g. "KEGG:C123" --> "C123"
+    UniProtKB --> split by colon; use the second part as input value
+    UBERON --> as is
+    CL --> as is (e.g "CL:0000492")
+    GO --> as is
+    DOID --> as is
+    OMIM --> split by colon; use the second part as input value
+    HP --> as is
+    NCBIGene --> split bycolon; use the second part as input value
+    REACT --> split bycolon; use the second part as input value
+    ChEMBL --> ^CHEMBL\d+$, e.g. "ChEMBL:123" --> "CHEMBL123"
+    """
+
+    curie_prefix, curie_value = curie_id.split(":")
+    if curie_prefix in ("KEGG", "UniProtKB", "OMIM", "NCBIGene", "REACT"):
+        bte_input_value = curie_value
+    elif curie_prefix == "ChEMBL":
+        bte_input_value = "CHEMBL" + curie_value
+    else:
+        bte_input_value = curie_id  # as is
+
+    bte_input_prefix = _input_prefix_map.get(curie_prefix, None)
+    if bte_input_prefix is None:
+        raise ValueError("Cannot find BioThings Explorer prefix for curie prefix {}".format(curie_prefix))
+
+    return bte_input_prefix, bte_input_value
--- test_RTXCurieUtil.py
+++ test_RTXCurieUtil.py
@@ -1,9 +1,25 @@
 import unittest
+import RTXCurieUtil as rcu
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_convert_rtx_curie_id_to_bt_explorer_input(self):
+        test_data = {
+            "KEGG:C00022": ("kegg.compound", "C00022"),
+            "UniProtKB:P01358": ("uniprot", "P01358"),
+            "UBERON:0004476": ("uberon", "UBERON:0004476"),
+            "CL:0000492": ("cl", "CL:0000492"),
+            "GO:0097289": ("go", "GO:0097289"),
+            "DOID:3965": ("do", "DOID:3965"),
+            "OMIM:100100": ("omim.disease", "100100"),
+            "HP:0011515": ("hp", "HP:0011515"),
+            "NCBIGene:1008470860": ("ncbigene", "1008470860"),
+            "REACT:R-HSA-703260": ("reactome.pathway", "R-HSA-703260"),
+            "ChEMBL:1200766": ("chembl.compound", "CHEMBL1200766")
+        }
+
+        for input, output in test_data.items():
+            self.assertEqual(rcu.convert_rtx_curie_id_to_bt_explorer_input(input), output),
 
 
 if __name__ == '__main__':
