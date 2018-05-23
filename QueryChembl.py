f05cdda76cba1a5969b9d6eac298111e91c6e92b
kevinxin90 <kevinxin@scripps.edu>
2018-05-23 15:53:25 -0400

    modify chembl to support negative case

--- QueryChembl.py
+++ QueryChembl.py
@@ -1,6 +1,3 @@
-import requests
-import sys
-
 from QueryBioThingsExplorer import QueryBioThingsExplorer
 
 class QueryChEMBL:
@@ -10,8 +7,11 @@ class QueryChEMBL:
 
     def get_chembl_ids_for_drug(self, drug_name):
         chembl_results = self.biothings_explorer.send_query_get(input_prefix='drugname', output_prefix="chembl.compound", input_value=drug_name)
-        res_chembl_set = set([_doc['output']['object']['id'].split(':')[-1]for _doc in chembl_results['data'] if _doc['endpoint'] == "https://www.ebi.ac.uk/chembl/api/data/compound_record"])
-        return res_chembl_set
+        if chembl_results:
+            res_chembl_set = set([_doc['output']['object']['id'].split(':')[-1] for _doc in chembl_results['data'] if _doc['endpoint'] == "https://www.ebi.ac.uk/chembl/api/data/compound_record"])
+            return res_chembl_set
+        else:
+            return None
 
     def get_target_uniprot_ids_for_chembl_id(self, chembl_id):
         target_predictions_list = self.biothings_explorer.send_query_get(input_prefix='chembl.compound', output_prefix='uniprot', input_value=chembl_id)
@@ -23,12 +23,16 @@ class QueryChEMBL:
     def get_target_uniprot_ids_for_drug(self, drug_name):
         chembl_ids_for_drug = QueryChEMBL().get_chembl_ids_for_drug(drug_name)
         res_uniprot_ids = dict()
-        for chembl_id in chembl_ids_for_drug:
-#            print(chembl_id)
-            uniprot_ids_dict = QueryChEMBL().get_target_uniprot_ids_for_chembl_id(chembl_id)
-            for uniprot_id in uniprot_ids_dict.keys():
-                res_uniprot_ids[uniprot_id] = uniprot_ids_dict[uniprot_id]
-        return res_uniprot_ids
+        if chembl_ids_for_drug:
+            for chembl_id in chembl_ids_for_drug:
+    #            print(chembl_id)
+                uniprot_ids_dict = self.get_target_uniprot_ids_for_chembl_id(chembl_id)
+                if uniprot_ids_dict:
+                    for uniprot_id in uniprot_ids_dict.keys():
+                        res_uniprot_ids[uniprot_id] = uniprot_ids_dict[uniprot_id]
+            return res_uniprot_ids
+        else:
+            return res_uniprot_ids
     
     def test(self):
         print(QueryChEMBL().get_chembl_ids_for_drug('acetaminophen'))
